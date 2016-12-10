34a507c443288377a716b243fd405b2d71396905
Andrew Garcia <runandrew@gmail.com>
2016-12-09 20:11:07 -0500

    initial project commit

---
+++ browser/react/action-creators/todo.js
@@ -0,0 +1,25 @@
+// Actions creators for To Do
+
+// Required files
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from '../constants';
+
+export const addTodo = (text) => {
+    return {
+        type: ADD_TODO,
+        text
+    };
+};
+
+export const toggleTodo = (index) => {
+    return {
+        type: TOGGLE_TODO,
+        index
+    };
+};
+
+export const setVisibilityFilter = (filter) => {
+    return {
+        type: SET_VISIBILITY_FILTER,
+        filter
+    };
+};
---
+++ browser/react/constants.js
@@ -0,0 +1,11 @@
+// To do action constants
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+// To do other constants
+export const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
